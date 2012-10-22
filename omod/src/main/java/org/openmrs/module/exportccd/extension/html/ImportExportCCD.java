aff6fde1930391c72fe01d0d51e5ed29e69e5fd2
Hui Xiao <hxiao@regenstrief.org>
2012-10-22 16:18:32 +0000

    Be able to import CCD from PHR patient dashboard

---
+++ omod/src/main/java/org/openmrs/module/exportccd/extension/html/ImportExportCCD.java
@@ -0,0 +1,82 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.exportccd.extension.html;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openmrs.api.context.Context;
+import org.openmrs.module.Extension;
+import org.openmrs.module.web.extension.PatientDashboardTabExt;;
+
+/**
+ * This class defines the links that will appear on the administration page under the
+ * "basicmodule.title" heading. This extension is enabled by defining (uncommenting) it in the
+ * /metadata/config.xml file.
+ */
+public class ImportExportCCD extends PatientDashboardTabExt {
+	
+	/**
+	 * default constructor: set display order attribute
+	 */
+	public ImportExportCCD() {
+		super();
+		//String order = Context.getAdministrationService().getGlobalProperty("lancearmstrong.PatientCommunities.displayorder");
+		//this.setOrder(Integer.valueOf(order==null? "4": order));
+	}
+
+	/**
+	 * @see org.openmrs.module.web.extension.AdministrationSectionExt#getMediaType()
+	 */
+	public Extension.MEDIA_TYPE getMediaType() {
+		return Extension.MEDIA_TYPE.html;
+	}
+	
+	/**
+     * @see org.openmrs.module.web.extension.PatientDashboardTabExt#getPortletUrl()
+     */
+    @Override
+    public String getPortletUrl() {
+	    // TODO Auto-generated method stub
+	    return "importExportCCD";
+    }
+
+	/**
+     * @see org.openmrs.module.web.extension.PatientDashboardTabExt#getRequiredPrivilege()
+     */
+    @Override
+    public String getRequiredPrivilege() {
+	    // TODO Auto-generated method stub
+	    return null;
+    }
+
+	/**
+     * @see org.openmrs.module.web.extension.PatientDashboardTabExt#getTabId()
+     */
+    @Override
+    public String getTabId() {
+	    // TODO Auto-generated method stub
+	    return "importexportCCD";
+    }
+
+	/**
+     * @see org.openmrs.module.web.extension.PatientDashboardTabExt#getTabName()
+     */
+    @Override
+    public String getTabName() {
+	    // TODO Auto-generated method stub
+	    return "My CCD";
+    }
+	
+}
---
+++ omod/src/main/webapp/portlets/importExportCCD.jsp
@@ -0,0 +1,26 @@
+<%@ include file="/WEB-INF/view/module/personalhr/template/include.jsp" %>
+<personalhr:require privilege="PHR Authenticated" otherwise="/phr/login.htm" redirect="/module/exportccd/portlets/importExportCCD.htm" />
+
+<div class="tooltipPhr">
+<spring:message code="exportccd.tooltip.ccd"/>
+</div>
+<div id="importexportccd_div">
+<br/>
+<h3>Import/Export My CCD</h3>
+<br/>
+ 
+<table>
+	<tbody>
+		<tr>
+			<td>				
+				<a href="../module/exportccd/fileupload.htm">Import CCD</a>
+			</td>
+			<td>
+			<td>				
+				<a href="../module/exportccd/exportPatient.form">Export CCD</a>
+			</td>
+		</tr>
+	</tbody>
+</table>
+</br>
+</div>
